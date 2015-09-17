ecec7ccb01f5b2e8a5f431171b2c3cc9c330963c
BootStorm <james@forrestclan.com>
2015-09-17 05:04:05 +0000

    service and conf files for apache

---
+++ modules/apache/manifests/init.pp
@@ -0,0 +1,31 @@
+class apache {
+
+  package { 'httpd':
+    ensure => installed,
+  }
+
+  file { '/var/www':
+    ensure => directory,
+  }
+
+  file { '/var/www/html':
+    ensure => directory,
+  }
+
+  file { '/var/www/html/index.html':
+    ensure => file,
+    source => 'puppet:///modules/apache/index.html',
+  }
+
+  file { '/etc/httpd/conf/httpd.conf':
+    ensure => file,
+    source => 'puppet:///modules/apache/httpd.conf',
+    notify => Service['httpd'],
+  }
+
+  service {'httpd':
+    ensure  => running,
+    require => Package['httpd'],
+  }
+
+}
